c35d77a0d2a3efe0162b9264a798b04c3347a839
Himanshu Pandey <154069821+Himanshu817324@users.noreply.github.com>
2024-10-28 02:48:00 +0530

    Adding  projects annd certificates section

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ function openTab(tabName) {
   event.currentTarget.classList.add("activeLink");
   document.getElementById(tabName).classList.add("activeTab")
 }
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
